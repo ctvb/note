854fede00f387926f465d97676a79bc516101835
ctvb <deenicks@hotmail.com>
2023-04-09 14:04:03 -0500

    tutor session did a lot

---
+++ 02-Challenge/Develop/routes/notes.js
@@ -0,0 +1,33 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the tips
+//http://localhost:3000/api/tips/
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for tips`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+//http://localhost:3000/api/tips/
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title, text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+module.exports = notes;
